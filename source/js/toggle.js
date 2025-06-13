0f090e04d033b3a522981177fc959dc9e7101690
nicolobolzon <nicolo.bolzon@gmail.com>
2025-06-13 10:38:42 +0200

    Modifiche varie

--- source/js/toggle.js
+++ source/js/toggle.js
@@ -6,20 +6,34 @@ document.addEventListener("DOMContentLoaded", function(){
 // GESTIONE DEL MENU AD HAMBURGER
 function aggiustaMenuResize(){
 
-    let btn_menu = document.getElementById("menu-button");
+    const btn_menu = document.getElementById("menu-button");
 
     if(btn_menu){
-        let menu = document.getElementById("menu");
-        let links = menu.querySelectorAll("a");
+        const menu = document.getElementById("menu");
+        const links = menu.querySelectorAll("a");
 
-        //cambia se menu visibile o nascosto
+        // Imposta stato iniziale
+        if(!btn_menu.hasAttribute("data-hidden")){
+            btn_menu.setAttribute("data-hidden", "true"); // = menu chiuso all’inizio
+            menu.style.display = "none";
+        }
+
+        // Funzione toggle apertura/chiusura menu
         function menuApriChiudi(){
             if(btn_menu.getAttribute("data-hidden")==="false"){
+                // Chiudi menu
+                btn_menu.setAttribute("data-hidden", "true");
+                menu.style.display = "none";
 
             } else {
-
+                // Apri menu
+                btn_menu.setAttribute("data-hidden", "false");
+                menu.style.display = "block";
             }
         }
+
+        // Evento click sul bottone
+        btn_menu.addEventListener("click", menuApriChiudi);
 
     }
 }
