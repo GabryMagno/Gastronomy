fa5f315d2a2a04ab67107df1a426896f252fe94c
GabryMagno <g061201magno@gmail.com>
2025-08-18 00:40:28 +0200

    Prima pagina errore (403)

---
+++ sito/php/403.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "db.php";
+use Utilities\DB;
+$db = new DB;
+
+$pagina = file_get_contents("./html/403.html");// Carica il template HTML della pagina 403
+
+if($db->isUserLog()!=false) {
+    echo str_replace("{{accesso-profilo}}","<a href=\"user-profile.php\">PROFILO</a>",$paginaHtml);//accesso profilo andrà messo in tutte le pagine al posto del link areariservata.html
+} else {
+    echo str_replace("{{accesso-profilo}}","<a href=\"areariservata.php\"><span lang=\"en\">LOGIN</span></a>",$paginaHtml); 
+}
+
+?>
