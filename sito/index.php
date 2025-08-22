de5249ac402a3d88494efc3e379ca104c7a145b2
nicolobolzon <nicolo.bolzon@gmail.com>
2025-08-22 10:55:18 +0200

    Aggiornato problema menu non funzionante mobile

--- sito/index.php
+++ sito/index.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once "php/db.php";
 
 $db = new DB;
@@ -7,11 +6,10 @@ $db = new DB;
 $pagina = file_get_contents("html/index.html");
 
 if($db->isUserLog()!=false) {
-    echo str_replace("[to-profile]","<a href=\"user-profile.php\">Profilo</a>",$pagina);
+    $pagina = str_replace("[to-profile]","<a href=\"user-profile.php\">Profilo</a>",$pagina);
 } else {
-    echo str_replace("[to-profile]","<a href=\"login.php\"><span lang=\"en\">Login</span></a>",$pagina); 
+    $pagina = str_replace("[to-profile]","<a href=\"login.php\"><span lang=\"en\">Login</span></a>",$pagina); 
 }
 
 echo $pagina;
-
 ?>
