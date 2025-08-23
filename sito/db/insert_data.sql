f8a1e422bcd6f15515d92bb5a1e5745c687ad077
nicolobolzon <nicolo.bolzon@gmail.com>
2025-08-23 14:02:16 +0200

    Aggiunta prodotti

--- sito/db/insert_data.sql
+++ sito/db/insert_data.sql
@@ -19,32 +19,28 @@ VALUES
 ('Gnocchi Sorrentina', 'primo', 'porzione', 1, 6, 'Gnocchi al forno con pomodoro e mozzarella', 7.00, TRUE, 'assets/img/prodotti/gnocchi_sorrentina.webp'),
 
 -- SECONDI
-
-
-('Pollo Arrosto', 'secondo', 'pezzo', 1, 4, 'Pollo intero arrosto con erbe aromatiche', 12.00, TRUE, 'assets/img/pollo_arrosto.jpg'),
-('Scaloppine al Limone', 'secondo', 'porzione', 1, 6, 'Fettine di vitello al limone', 9.00, TRUE, 'assets/img/scaloppine.jpg'),
-('Baccalà alla Vicentina', 'secondo', 'porzione', 1, 6, 'Filetto di baccalà stufato con cipolle e latte', 11.00, TRUE, 'assets/img/baccala.jpg'),
-('Involtini di Carne', 'secondo', 'pezzo', 1, 8, 'Carne di vitello ripiena e cotta al forno', 4.00, TRUE, 'assets/img/involtini.jpg'),
-('Arrosto di Tacchino', 'secondo', 'porzione', 1, 6, 'Fettine di tacchino al forno con erbe aromatiche', 8.50, TRUE, 'assets/img/arrosto_tacchino.jpg'),
-('Polpette al Sugo', 'secondo', 'pezzo', 2, 12, 'Polpette di carne in salsa di pomodoro', 1.50, TRUE, 'assets/img/polpette.jpg'),
+('Pollo Arrosto', 'secondo', 'pezzo', 1, 4, 'Pollo intero arrosto con erbe aromatiche', 12.00, TRUE, 'assets/img/pollo_arrosto.webp'),
+('Scaloppine al Limone', 'secondo', 'porzione', 1, 6, 'Fettine di vitello al limone', 9.00, TRUE, 'assets/img/scaloppine.webp'),
+('Baccalà alla Vicentina', 'secondo', 'porzione', 1, 6, 'Filetto di baccalà stufato con cipolle e latte', 11.00, TRUE, 'assets/img/baccala.webp'),
+('Involtini di Carne', 'secondo', 'pezzo', 1, 8, 'Carne di vitello ripiena e cotta al forno', 4.00, TRUE, 'assets/img/involtini.webp'),
+('Arrosto di Tacchino', 'secondo', 'porzione', 1, 6, 'Fettine di tacchino al forno con erbe aromatiche', 8.50, TRUE, 'assets/img/arrosto_tacchino.webp'),
+('Polpette al Sugo', 'secondo', 'pezzo', 2, 12, 'Polpette di carne in salsa di pomodoro', 1.50, TRUE, 'assets/img/polpette.webp'),
+('Arrosto di Maiale', 'secondo', 'kg', 1, 5, 'Arrosto di maiale cotto lentamente, prezzo al kg', 24.00, TRUE, 'assets/img/arrosto_maiale.webp'),
 
 -- CONTORNI
-('Patate al Forno', 'contorno', 'porzione', 1, 6, 'Patate al forno croccanti con rosmarino', 4.00, TRUE, 'assets/img/patate_forno.jpg'),
-('Verdure Grigliate', 'contorno', 'porzione', 1, 6, 'Misto di zucchine, melanzane e peperoni grigliati', 4.50, TRUE, 'assets/img/verdure_grigliate.jpg'),
-('Insalata Mista', 'contorno', 'porzione', 1, 6, 'Insalata fresca con pomodori, cetrioli e lattuga', 3.50, TRUE, 'assets/img/insalata.jpg'),
-('Spinaci al Burro', 'contorno', 'porzione', 1, 6, 'Spinaci freschi saltati al burro', 3.50, TRUE, 'assets/img/spinaci.jpg'),
-('Caponata Siciliana', 'contorno', 'porzione', 1, 6, 'Melanzane, peperoni e cipolle in agrodolce', 5.00, TRUE, 'assets/img/caponata.jpg'),
-('Zucchine Trifolate', 'contorno', 'porzione', 1, 6, 'Zucchine saltate in padella con aglio e prezzemolo', 4.00, TRUE, 'assets/img/zucchine.jpg'),
+('Patate al Forno', 'contorno', 'porzione', 1, 6, 'Patate al forno croccanti con rosmarino', 4.00, TRUE, 'assets/img/patate_forno.webp'),
+('Verdure Grigliate', 'contorno', 'porzione', 1, 6, 'Misto di zucchine, melanzane, pomodori, asparagi e peperoni grigliati', 4.50, TRUE, 'assets/img/verdure_grigliate.webp'),
+('Insalata Mista', 'contorno', 'porzione', 1, 6, 'Insalata fresca con pomodori, cetrioli e lattuga', 3.50, TRUE, 'assets/img/insalata.webp'),
+('Spinaci al Burro', 'contorno', 'porzione', 1, 6, 'Spinaci freschi saltati al burro', 3.50, TRUE, 'assets/img/spinaci.webp'),
+('Caponata Siciliana', 'contorno', 'porzione', 1, 6, 'Melanzane, peperoni e cipolle in agrodolce', 5.00, TRUE, 'assets/img/caponata.webp'),
+('Zucchine Trifolate', 'contorno', 'porzione', 1, 6, 'Zucchine saltate in padella con aglio e prezzemolo', 4.00, TRUE, 'assets/img/zucchine.webp'),
+('Insalata di Farro', 'contorno', 'kg', 1, 4, 'Insalata di farro con verdure di stagione', 15.00, TRUE, 'assets/img/insalata_farro.webp'),
 
 -- DOLCI
-('Tiramisù', 'dolce', 'porzione', 1, 6, 'Classico dolce al cucchiaio con savoiardi, caffè e mascarpone', 4.50, TRUE, 'assets/img/tiramisu.jpg'),
-('Panna Cotta', 'dolce', 'porzione', 1, 6, 'Panna cotta con topping di frutti di bosco', 4.00, TRUE, 'assets/img/pannacotta.jpg'),
-('Cassata Siciliana', 'dolce', 'porzione', 1, 6, 'Dolce tipico con ricotta, pan di spagna e canditi', 5.00, TRUE, 'assets/img/cassata.jpg'),
-('Crostata alla Marmellata', 'dolce', 'pezzo', 1, 8, 'Frolla con confettura di albicocca', 3.00, TRUE, 'assets/img/crostata.jpg'),
-('Bignè alla Crema', 'dolce', 'pezzo', 2, 12, 'Pasta choux ripiena di crema pasticcera', 1.50, TRUE, 'assets/img/bigne.jpg'),
-('Cheesecake ai Frutti Rossi', 'dolce', 'porzione', 1, 6, 'Cheesecake con topping ai frutti rossi', 4.50, TRUE, 'assets/img/cheesecake.jpg'),
-
--- PRODOTTI A PESO (KG)
-('Arrosto di Maiale', 'secondo', 'kg', 1, 5, 'Arrosto di maiale cotto lentamente, prezzo al kg', 24.00, TRUE, 'assets/img/arrosto_maiale_kg.jpg'),
-('Biscotti Artigianali', 'dolce', 'kg', 1, 3, 'Selezione di biscotti di frolla artigianali, prezzo al kg', 22.00, TRUE, 'assets/img/biscotti_kg.jpg'),
-('Insalata di Farro', 'contorno', 'kg', 1, 4, 'Insalata di farro con verdure di stagione, prezzo al kg', 15.00, TRUE, 'assets/img/insalata_farro_kg.jpg');
+('Tiramisù', 'dolce', 'porzione', 1, 6, 'Classico dolce al cucchiaio con savoiardi, caffè e mascarpone', 4.50, TRUE, 'assets/img/tiramisu.webp'),
+('Panna Cotta', 'dolce', 'porzione', 1, 6, 'Panna cotta con topping di frutti di bosco', 4.00, TRUE, 'assets/img/pannacotta.webp'),
+('Cassata Siciliana', 'dolce', 'porzione', 1, 6, 'Dolce tipico con ricotta, pan di spagna e canditi', 5.00, TRUE, 'assets/img/cassata.webp'),
+('Crostata alla Marmellata', 'dolce', 'pezzo', 1, 8, 'Frolla con confettura di albicocca', 3.00, TRUE, 'assets/img/crostata.webp'),
+('Bignè alla Crema', 'dolce', 'pezzo', 2, 12, 'Pasta choux ripiena di crema pasticcera', 1.50, TRUE, 'assets/img/bigne.webp'),
+('Biscotti Artigianali', 'dolce', 'kg', 1, 3, 'Selezione di biscotti di frolla artigianali', 18.00, TRUE, 'assets/img/biscotti.webp'),
+('Cheesecake ai Frutti Rossi', 'dolce', 'porzione', 1, 6, 'Cheesecake con topping ai frutti rossi', 4.50, TRUE, 'assets/img/cheesecake.webp');
