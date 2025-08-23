b33236e7d6d3baa2998ef4efe9cd9889e586f37d
nicolobolzon <nicolo.bolzon@gmail.com>
2025-08-23 15:10:20 +0200

    Primo aggiornamento user-profile

--- sito/user-profile.php
+++ sito/user-profile.php
@@ -6,6 +6,20 @@ $db = new DB;
 
 $pagina = file_get_contents("html/user-profile.html");
 
+$isLogged_first = $db->isUserLog();//controllo se l'utente è loggato
+$isLogged = $db->UserUsername();//recupero l'username dell'utente loggato
+
+/*
+if ((is_bool($isLogged_first) && $isLogged_first == false)||(is_bool($isLogged) && $isLogged == false)) {//controllo se l'utente non è loggato(sia con id che con username)
+    header('Location: login.php');
+    exit();
+}
+*/
+
+$userInfo = $db->GetUserInfo();//recupero le informazioni dell'utente loggato
+
+
+
 echo $pagina;
 
 ?>
