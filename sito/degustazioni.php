c6fd194481d40ba28a1f86ea770f75d647b4cd37
nicolobolzon <nicolo.bolzon@gmail.com>
2025-08-25 16:40:01 +0200

    aggiunta degustazioni.php

---
+++ sito/degustazioni.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "php/db.php";
+
+$db = new DB;
+
+$pagina = file_get_contents("html/degustazioni.html");
+
+$isUserLogged = $db->isUserLog(); //controllo se l'utente è loggato
+$isLogged = $db->UserUsername(); //recupero l'username dell'utente loggato
+
+if ($isUserLogged!=false) {//se l'utente è loggato, mostra il suo profilo
+    $pagina=str_replace("[to-profile]","<a href=\"user-profile.php\">Profilo</a>",$pagina);//se l'utente è loggato, mostra il link al suo profilo
+    $userInfo=$db->getUserInfo();//ottiene le informazioni dell'utente loggato
+    if (is_string($userInfo) && (strcmp($userInfo,"Execution error")==0 || strcmp($userInfo,"User not found")==0 || strcmp($userInfo,"Connection error")==0)) {//se c'è un errore nell'ottenere le informazioni dell'utente, reindirizza alla pagina di errore
+        header('Location: 500.php');
+        exit();
+    } else if(is_string($userInfo) && strcmp($userInfo,"User Is not logged")==0) {
+        header('Location: login.php');
+        exit();
+    }
+} else {//se l'utente non è loggato, mostra il link per il login
+    $pagina=str_replace("[to-profile]","<a href=\"login.php\"><span lang=\"en\">Login</span></a>",$pagina);
+}
+
+echo $pagina;
+
+?>
