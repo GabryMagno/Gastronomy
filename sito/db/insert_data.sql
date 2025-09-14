d4e9c0bf08542f14baef1695070391c529dd3265
GabryMagno <g061201magno@gmail.com>
2025-09-14 13:39:41 +0200

    Aggiornamento date degustazioni

--- sito/db/insert_data.sql
+++ sito/db/insert_data.sql
@@ -488,8 +488,8 @@ VALUES
 
 -- PRESENTI
 (2, 'Tagliere di Salumi in degustazione: un assaggio delle nostre migliori selezioni di salumi nostrani accompagnati da pane fresco. Un incontro tra tradizione e convivialità. Servizio e bevanda inclusi.', 0, '2025-09-10', '2025-10-10', 14.00),
-(9, 'Risotto ai Funghi: una degustazione dedicata al gusto autentico dei porcini freschi, servito in porzioni assaggio per apprezzarne cremosità e intensità. Degustazione completa con calice di vino.', 4, '2025-08-15', '2025-09-15', 18.00),
-(18, 'Arrosto di Tacchino in degustazione: fettine cotte al forno con erbe aromatiche, leggere ma ricche di sapore. Ideale per chi cerca equilibrio e tradizione, con servizio e bevanda inclusi.', 2, '2025-08-25', '2025-09-25', 20.00),
+(9, 'Risotto ai Funghi: una degustazione dedicata al gusto autentico dei porcini freschi, servito in porzioni assaggio per apprezzarne cremosità e intensità. Degustazione completa con calice di vino.', 4, '2025-08-25', '2025-09-25', 18.00),
+(18, 'Arrosto di Tacchino in degustazione: fettine cotte al forno con erbe aromatiche, leggere ma ricche di sapore. Ideale per chi cerca equilibrio e tradizione, con servizio e bevanda inclusi.', 2, '2025-08-30', '2025-09-30', 20.00),
 
 -- PASSATE
 (29, 'Panna Cotta con frutti di bosco: una degustazione dedicata a chi ama i dolci freschi e delicati, con la possibilità di scoprire abbinamenti sorprendenti. Comprende bevanda calda o fresca.', 0, '2025-07-18', '2025-08-18', 12.00);
